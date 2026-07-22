76f402283ca71e61ac52e926824a8ac99847f894
Komyyy <pol_tta@outlook.jp>
2026-07-23 00:12:14 +0900

    refactor: compile the statement of Ado

--- Ado/Nilpotent.lean
+++ Ado/Nilpotent.lean
@@ -12,7 +12,7 @@ public import Mathlib
 
 section ForMathlib
 
-public section LieModuleTransferInstance
+@[expose] public section LieModuleTransferInstance
 
 variable (R L : Type*) {M₁ M₂ : Type*}
 
@@ -73,9 +73,39 @@ def Equiv.lieModuleEquiv
   { e.linearEquiv R with
     map_lie' {x m} := by unfold_projs; simp }
 
+open Function LieModule
+
+variable {R L}
+variable [CommRing R] [LieRing L] [LieAlgebra R L]
+variable [AddCommGroup M₁] [AddCommGroup M₂] [Module R M₁] [Module R M₂]
+variable [LieRingModule L M₁] [LieRingModule L M₂] [LieModule R L M₁] [LieModule R L M₂]
+
+lemma Function.Injective.isFaithful [IsFaithful R L M₁] (f : M₁ →ₗ⁅R,L⁆ M₂) (hf : Injective f) :
+    IsFaithful R L M₂ := by
+  rw [LieModule.isFaithful_iff']
+  intro x hx
+  apply ext_of_isFaithful (R := R) (M := M₁)
+  intro m
+  rw [zero_lie, ← hf.eq_iff, f.map_lie, map_zero, hx]
+
+lemma LieModuleEquiv.isFaithful_iff (e : M₁ ≃ₗ⁅R,L⁆ M₂) :
+    IsFaithful R L M₁ ↔ IsFaithful R L M₂ where
+  mp _ := e.toEquiv.injective.isFaithful e.toLieModuleHom
+  mpr _ := e.symm.toEquiv.injective.isFaithful e.symm.toLieModuleHom
+
+omit [LieAlgebra R L] [LieModule R L M₁] [LieModule R L M₂] in
+@[simp]
+lemma LieModuleEquiv.map_lie (e : M₁ ≃ₗ⁅R,L⁆ M₂) (x : L) (m : M₁) : e ⁅x, m⁆ = ⁅x, e m⁆ :=
+  e.toLieModuleHom.map_lie x m
+
+lemma LieModuleEquiv.isNilpotent_iff (e : M₁ ≃ₗ⁅R,L⁆ M₂) : IsNilpotent L M₁ ↔ IsNilpotent L M₂ :=
+  Equiv.lieModule_isNilpotent_iff (f := .refl) (g := e.toLinearEquiv) (by simp)
+
 end LieModuleTransferInstance
 
 public noncomputable section LieModuleShrink
+
+open LieModule
 
 universe u
 
@@ -96,11 +126,102 @@ variable [CommRing R] [LieRing L] [LieAlgebra R L] [AddCommGroup M] [Module R M]
 def lieModuleEquiv : Shrink.{u} M ≃ₗ⁅R,L⁆ M :=
   (equivShrink M).symm.lieModuleEquiv R L
 
+variable {R L}
+
+@[simp]
+lemma isFaithful_iff : IsFaithful R L (Shrink.{u} M) ↔ IsFaithful R L M :=
+  (lieModuleEquiv R L).isFaithful_iff
+
+instance [IsFaithful R L M] : IsFaithful R L (Shrink.{u} M) :=
+  isFaithful_iff.mpr ‹IsFaithful R L M›
+
+@[simp]
+lemma isNilpotent_iff : IsNilpotent L (Shrink.{u} M) ↔ IsNilpotent L M :=
+  (lieModuleEquiv ℤ L).isNilpotent_iff
+
+instance [IsNilpotent L M] : IsNilpotent L (Shrink.{u} M) :=
+  isNilpotent_iff.mpr ‹IsNilpotent L M›
+
 end Shrink
 
 end LieModuleShrink
 
 end ForMathlib
+
+public section Statement
+
+universe u
+
+open LieAlgebra LieModule
+
+namespace LieAlgebra
+
+structure BundledAdoSpace (K : Type u) (𝔤 : Type*)
+    [Field K] [CharZero K] [LieRing 𝔤] [LieAlgebra K 𝔤] where
+  mk' ::
+  (V : Type u)
+  [instAddCommGroup : AddCommGroup V]
+  [instModule : Module K V]
+  [instFiniteDimentional : FiniteDimensional K V]
+  [instLieRingModule : LieRingModule 𝔤 V]
+  [instLieModule : LieModule K 𝔤 V]
+  [instIsFaithful : IsFaithful K 𝔤 V]
+  [instIsNilpotentMaxNilpotentIdeal : IsNilpotent (maxNilpotentIdeal K 𝔤) V]
+
+attribute [instance]
+  BundledAdoSpace.instAddCommGroup
+  BundledAdoSpace.instModule
+  BundledAdoSpace.instFiniteDimentional
+  BundledAdoSpace.instLieRingModule
+  BundledAdoSpace.instLieModule
+  BundledAdoSpace.instIsFaithful
+  BundledAdoSpace.instIsNilpotentMaxNilpotentIdeal
+
+@[expose]
+noncomputable def BundledAdoSpace.mk {K : Type u} {𝔤 : Type*}
+    [Field K] [CharZero K] [LieRing 𝔤] [LieAlgebra K 𝔤]
+    (V : Type*) [AddCommGroup V] [Module K V] [FiniteDimensional K V] [LieRingModule 𝔤 V]
+    [LieModule K 𝔤 V] [IsFaithful K 𝔤 V] [IsNilpotent (maxNilpotentIdeal K 𝔤) V] :
+    BundledAdoSpace K 𝔤 :=
+  haveI : Small.{u} V := Module.Finite.small K V
+  { V := Shrink.{u} V }
+
+class IsAdo (K : Type u) (𝔤 : Type*) [Field K] [CharZero K] [LieRing 𝔤] [LieAlgebra K 𝔤]
+    : Prop where
+  intro ::
+  nonempty_bundledAdoSpace : Nonempty (BundledAdoSpace K 𝔤)
+
+end LieAlgebra
+
+def AdoSpace (K : Type u) (𝔤 : Type*)
+    [Field K] [CharZero K] [LieRing 𝔤] [LieAlgebra K 𝔤] [ia : IsAdo K 𝔤] : Type u :=
+  ia.nonempty_bundledAdoSpace.some.V
+
+variable (K : Type u) (𝔤 : Type*)
+variable [Field K] [CharZero K] [LieRing 𝔤] [LieAlgebra K 𝔤] [ia : IsAdo K 𝔤]
+
+@[no_expose]
+noncomputable instance : AddCommGroup (AdoSpace K 𝔤) :=
+  inferInstanceAs (AddCommGroup ia.nonempty_bundledAdoSpace.some.V)
+
+@[no_expose]
+noncomputable instance : Module K (AdoSpace K 𝔤) :=
+  inferInstanceAs (Module K ia.nonempty_bundledAdoSpace.some.V)
+
+instance : FiniteDimensional K (AdoSpace K 𝔤) :=
+  inferInstanceAs (FiniteDimensional K ia.nonempty_bundledAdoSpace.some.V)
+
+@[no_expose]
+noncomputable instance : LieRingModule 𝔤 (AdoSpace K 𝔤) :=
+  inferInstanceAs (LieRingModule 𝔤 ia.nonempty_bundledAdoSpace.some.V)
+
+instance : LieModule K 𝔤 (AdoSpace K 𝔤) :=
+  inferInstanceAs (LieModule K 𝔤 ia.nonempty_bundledAdoSpace.some.V)
+
+instance : IsFaithful K 𝔤 (AdoSpace K 𝔤) :=
+  inferInstanceAs (IsFaithful K 𝔤 ia.nonempty_bundledAdoSpace.some.V)
+
+end Statement
 
 open Function Set Module LieAlgebra LieModule
 open scoped Pointwise
@@ -109,17 +230,14 @@ namespace LieAlgebra
 
 universe u
 
-variable {K : Type u} {𝔤 : Type*}
+variable {K 𝔤 : Type*}
 variable [Field K] [CharZero K] [LieRing 𝔤] [LieAlgebra K 𝔤] [FiniteDimensional K 𝔤]
 variable [LieRing.IsNilpotent 𝔤]
 
 -- TODO: 後でインスタンスにする
 attribute [local instance 100] LieRing.ofAssociativeRing
 
-public theorem ado_of_isNilpotent :
-    ∃ (V : Type u) (_ : AddCommGroup V) (_ : Module K V) (_ : FiniteDimensional K V)
-      (_ : LieRingModule 𝔤 V) (_ : LieModule K 𝔤 V),
-      IsFaithful K 𝔤 V ∧ LieModule.IsNilpotent 𝔤 V := by
+public theorem ado_of_isNilpotent : IsAdo K 𝔤 := by
   sorry
 
 end LieAlgebra
