646ccbe915f3f779e84ee50e4b44c314f0aa6bc9
Komyyy <pol_tta@outlook.jp>
2026-09-01 17:25:33 +0900

    feat: solvable Ado (1/N)

---
+++ Ado/Solvable.lean
@@ -0,0 +1,83 @@
+/-
+Copyright (c) 2026 Miyahara Kō. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Miyahara Kō
+-/
+module
+public import Ado.Nilpotent
+
+/-!
+## 可解 Lie 代数に対する Ado の定理
+-/
+
+namespace CounterExample.LieMaxNilpotentIdeal
+
+open LieAlgebra LieModule LieIdeal
+
+attribute [local instance 100] LieRing.ofAssociativeRing LieRingModule.ofAssociativeModule
+
+variable (p : ℕ) [Fact p.Prime]
+
+def ψ : ZMod p × ZMod p →ₗ⁅ZMod p⁆ LieDerivation (ZMod p) (GaloisField p p) (GaloisField p p) :=
+  sorry
+
+variable {K L : Type*}
+variable [Field K] [LieRing L] [LieAlgebra K L]
+
+lemma LieIdeal.lieIdealOf_maxNilpotentIdeal_eq_of_le
+    (I : LieIdeal K L) (hN : maxNilpotentIdeal K L ≤ I) :
+    maxNilpotentIdeal K I = lieIdealOf (maxNilpotentIdeal K L) I := by
+  impossible +levels by
+    push Not
+    obtain ⟨p, hp⟩ : ∃ p : ℕ, Fact p.Prime := ⟨2, by decide⟩
+    existsi GaloisField p p
+    sorry
+
+end CounterExample.LieMaxNilpotentIdeal
+
+#exit
+
+open Module LieAlgebra LieModule
+
+variable {K 𝔰 : Type*}
+variable [Field K] [CharZero K] [LieRing 𝔰] [LieAlgebra K 𝔰] [FiniteDimensional K 𝔰]
+variable [LieAlgebra.IsSolvable 𝔰]
+
+variable (K 𝔰) in
+lemma LieIdeal.exists_for_solStepAdoData_of_not_isLieAbelian (n : ℕ)
+    (h𝔫r : finrank K (𝔰 ⧸ maxNilpotentIdeal K 𝔰) = n + 1) :
+    ∃ 𝔞 : LieIdeal K 𝔰, finrank K (𝔞 ⧸ maxNilpotentIdeal K 𝔞) = n ∧ maxNilpotentIdeal K 𝔰 ≤ 𝔞 := by
+  sorry
+
+structure SolStepAdoData (K 𝔰 : Type*)
+    [Field K] [CharZero K] [LieRing 𝔰] [LieAlgebra K 𝔰] [FiniteDimensional K 𝔰]
+    [LieAlgebra.IsSolvable 𝔰] where
+  protected 𝔞 : LieIdeal K 𝔰
+  protected 𝔥 : LieSubalgebra K 𝔰
+  maxNilpotentIdeal_le_𝔞 : maxNilpotentIdeal K 𝔰 ≤ 𝔞
+  isCompl_toSubmodule : IsCompl 𝔞.toSubmodule 𝔥.toSubmodule
+  [instIsAdo𝔞 : IsAdo K 𝔞]
+
+axiom SolStepAdoData.isAdo (D : SolStepAdoData K 𝔰) : IsAdo K 𝔰
+
+public local instance LieAlgebra.IsAdo.of_isSolvable : IsAdo K 𝔰 := by
+  generalize hn : finrank K (𝔰 ⧸ maxNilpotentIdeal K 𝔰) = n
+  induction n generalizing 𝔰 with
+  | zero =>
+    rw [LieIdeal.finrank_quotient, Nat.sub_eq_zero_iff_le, ← not_lt, LieIdeal.finrank_lt_iff,
+      not_lt_top_iff, ← top_le_iff, ← LieIdeal.isNilpotent_iff_le_maxNilpotentIdeal,
+      isNilpotent_of_top_iff'] at hn
+    exact IsAdo.of_isNilpotent
+  | succ n hin =>
+    rsuffices ⟨D⟩ : Nonempty (SolStepAdoData K 𝔰)
+    · exact D.isAdo
+    obtain ⟨𝔞, hn𝔞, h𝔞⟩ := LieIdeal.exists_for_solStepAdoData_of_not_isLieAbelian K 𝔰 n hn
+    specialize hin hn𝔞
+    obtain ⟨𝔥, h𝔥₁⟩ : ∃ 𝔥 : LieSubalgebra K 𝔰, IsCompl 𝔞.toSubmodule 𝔥.toSubmodule := by
+      obtain ⟨𝔥', h𝔥'⟩ := exists_isCompl 𝔞.toSubmodule
+      stop
+      rw [← Submodule.finrank_add_eq_of_isCompl h𝔥', finrank_toSubmodule,
+        Nat.add_left_cancel_iff] at hn
+      existsi 𝔥'.toLieSubalgebraOfDimOne hn
+      exact h𝔥'
+    exact ⟨{ 𝔞, 𝔥, maxNilpotentIdeal_le_𝔞 := h𝔞, isCompl_toSubmodule := h𝔥₁ }⟩
